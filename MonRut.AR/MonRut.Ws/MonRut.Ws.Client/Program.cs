efbfb419dfcbf11775cf3a42a02e95c5ead3a04b
aalvarado <adan.alvarado7@gmail.com>
2011-04-03 01:04:03 -0800

    * Deleted person model * added client example

---
+++ MonRut.AR/MonRut.Ws/MonRut.Ws.Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonRut.Ws.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el nombre de una ruta");
+            string name = Console.ReadLine();
+            MonRutWs.MonRutWsSoapClient webservice = new MonRutWs.MonRutWsSoapClient("MonRutWsSoap");
+
+
+            try
+            {
+                webservice.AddRoute(name);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
